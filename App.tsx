5ef89acb4740fa70929c99c3d37a3b4db9d68ac2
Nazilah <163401104+zilah7@users.noreply.github.com>
2025-11-03 09:35:29 +0700

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { StatusBar } from 'react-native'
+import HomeScreen from './src/screens/HomeScreen'
+
+const App = () => {
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView style={{ flex: 1 }}>
+        <HomeScreen />
+      </SafeAreaView>
+    </GestureHandlerRootView>
+  )
+}
+
+export default App
